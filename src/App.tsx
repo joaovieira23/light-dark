e0108282baee46449d7113f347f3f28746e8c00e
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-11-12 17:09:20 -0300

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light';
+
+import GlobalStyle from './styles/global';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <Header />
+        <GlobalStyle />
+      </div>
+    </ThemeProvider>
   );
 }
 
