71ac458e5f07da0741430ae2c401cee7652b3efd
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-11-12 17:41:59 -0300

    feat: add theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { ThemeProvider } from 'styled-components';
 import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
 import Header from './Components/Header';
 
 function App() {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }, [theme]);
+
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme}>
       <div className="App">
-        <Header />
+        <Header toggleTheme={toggleTheme} />
         <GlobalStyle />
       </div>
     </ThemeProvider>
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -6,5 +6,6 @@ export const Container = styled.div`
   color: #FFF;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 30px;
 `;
